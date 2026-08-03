a4aebb151fdbc8d4b60069aaf9f21e47da6c4cab
yuribabuska <yuribabuska@gmail.com>
2026-08-03 15:09:23 -0300

    up to lesson 59 - Done with React Essentials

--- src/App.jsx
+++ src/App.jsx
@@ -40,23 +40,25 @@ function App() {
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
+
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
+            {CORE_CONCEPTS.map( (conceptItem) => (<CoreConcept key={conceptItem.title} {...conceptItem}/>) )}
+            
+            {/*  Two other ways: 
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept title={CORE_CONCEPTS[3].title}
               description={CORE_CONCEPTS[3].description}
               image={CORE_CONCEPTS[3].image}
-            />
+            /> */}
           </ul>
         </section>
         <section id="examples">
         <h2>Examples</h2>
         <menu>
-          <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-          <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-          <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-          <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+          <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+          <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+          <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
         </menu>
         {tabContent}
         
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,10 +1,10 @@
-export default function TabButton({children, onSelect}){
+export default function TabButton({children, onSelect, isSelected}){
 
-    
+
 
     return( 
     <li>
-        <button onClick={onSelect}>{children}</button>
+        <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
     </li>
 );
 }
